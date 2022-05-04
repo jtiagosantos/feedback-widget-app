37a26696d6822700e93384df468d6a7d3f1beeec
jtiagosantos <tiagosan040@gmail.com>
2022-05-04 16:30:14 -0400

    feat: implement route to create a feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 const PORT = 3333;
+
+app.use(express.json());
+
+app.post('/feedback/create', async (req: Request, res: Response) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
