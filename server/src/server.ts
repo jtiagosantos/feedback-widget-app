9c9b0fc84eaf3cea6534e66b8f7b07a4ef80d9df
jtiagosantos <tiagosan040@gmail.com>
2022-05-04 19:00:59 -0400

    chore: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes';
 
 const app = express();
 const PORT = 3333;
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(PORT, () => {
