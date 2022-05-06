3a90a3168072b19850a0abe4a95fe1a2c0e29722
jtiagosantos <tiagosan040@gmail.com>
2022-05-06 18:48:16 -0400

    chore: add PORT variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { routes } from './routes';
 
 const app = express();
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
